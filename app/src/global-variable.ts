aca1869d20945d2f525d120d9e30b629a2563a5a
sheng liang <ahliang51@hotmail.com>
2018-01-13 21:26:32 +0800

    added heroku

--- app/src/global-variable.ts
+++ app/src/global-variable.ts
@@ -1,2 +1,3 @@
 // export var apiUrl: string = 'http://localhost:3000';
-export var apiUrl: string = 'http://192.168.1.2:3000';
+// export var apiUrl: string = 'http://192.168.1.2:3000';
+export var apiUrl: string = 'https://retail-card.herokuapp.com';
