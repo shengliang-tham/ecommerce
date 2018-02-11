c94dc95524d47f1219d07df3f44ced81891de573
sheng liang <ahliang51@hotmail.com>
2018-02-11 18:39:39 +0800

    finalised layout for home

--- app/src/global-variable.ts
+++ app/src/global-variable.ts
@@ -1,3 +1,3 @@
 // export var apiUrl: string = 'http://localhost:3000';
-export var apiUrl: string = 'http://192.168.1.4:3000';
-// export var apiUrl: string = 'https://retail-card.herokuapp.com';
+// export var apiUrl: string = 'http://192.168.1.4:3000';
+export var apiUrl: string = 'https://retail-card.herokuapp.com';
